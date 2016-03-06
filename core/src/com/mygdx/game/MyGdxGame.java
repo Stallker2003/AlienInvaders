5a3412ea854d4588457baa6da91b9dff71e834f8
v1pire <bot.i.cow@gmail.com>
2016-03-07 01:38:23 +0300

    Добавил апдейт Его надо вынести в дальнейшем за приделы класса А еще лучше - слушателем

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,23 +5,38 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.sun.corba.se.impl.oa.poa.ActiveObjectMap;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture fly;
+	int n = 1;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("bkg.jpg");
+		fly = new Texture("fly.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.3f, 0.3f, 0.2f, .3f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		batch.draw(fly, 44+n, 77);
 		batch.end();
+
+		Update();
+
+	}
+
+	//Это вызывается каждый кадр из рендера формы(после)
+	void Update()
+	{
+		if(Gdx.input.getX()>Gdx.graphics.getWidth()/2)n++;
+		if(Gdx.input.getX()<Gdx.graphics.getWidth()/2)n--;
 	}
 }
