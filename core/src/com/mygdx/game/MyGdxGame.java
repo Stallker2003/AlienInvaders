4ce20343906b19b84360f1e5b821f74d174697c4
Stallker2003 <Stallker2003@gmail.com>
2016-03-07 13:39:47 +0200

    Поправка

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,9 +2,11 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.sun.corba.se.impl.oa.poa.ActiveObjectMap;
 
 import java.util.Iterator;
@@ -16,6 +18,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	int round = 0;
 	MapManager mapManager = new MapManager();
 	static long lastShot = 0;//для таймера
+	AlienFormation alienFormation = new AlienFormation();
 
 	StarShip starShip;
 	
